4da51fd9196453b9bcc9800cfb0f22203cf32a88
Pavel Antropov <p.m.antropov@gmail.com>
2022-08-13 21:47:15 +0400

    Write code in Program.cs to run a result program

--- src/qms-consoleapp/Program.cs
+++ src/qms-consoleapp/Program.cs
@@ -1,3 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-Console.WriteLine("Hello, World!");
+using pavelantropov.qms_consoleapp.Entities;
+using pavelantropov.qms_consoleapp.Repositories;
+using pavelantropov.qms_consoleapp.Services;
+
+var windowService = new WindowService(new WindowRepository());
+
+var serviceTypes = new List<ServiceType>
+{
+	new ("A", 5),
+	new ("B", 7),
+	new ("C", 10),
+	new ("D", 15),
+};
+
+windowService.Create(480, serviceTypes);
+windowService.Create(480, serviceTypes);
+windowService.Create(480, serviceTypes);
+
+var queueService = new QueueService(windowService);
+
+while (windowService.GetAll().Any(x => x.IsOpen))
+{
+	var serviceType = serviceTypes.ElementAt(new Random().Next(0, serviceTypes.Count));
+
+	queueService.GiveTicket(serviceType);
+}
