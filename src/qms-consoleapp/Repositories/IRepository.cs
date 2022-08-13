b551b73fe0c9c6d8dacd28f131d1276dffb765d4
Pavel Antropov <p.m.antropov@gmail.com>
2022-08-13 21:45:00 +0400

    Create a window repository

---
+++ src/qms-consoleapp/Repositories/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace pavelantropov.qms_consoleapp.Repositories;
+
+public interface IRepository<T> where T : class
+{
+	List<T> Items { get; set; }
+
+	void Add(T item);
+	void Update(int id, T item);
+	void Remove(int id);
+}
---
+++ src/qms-consoleapp/Repositories/WindowRepository.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+using pavelantropov.qms_consoleapp.Entities;
+
+namespace pavelantropov.qms_consoleapp.Repositories;
+
+public class WindowRepository : IRepository<Window>
+{
+	public WindowRepository()
+	{
+		Items = new List<Window>();
+	}
+
+	public List<Window> Items { get; set; }
+	public void Add(Window item)
+	{
+		throw new System.NotImplementedException();
+	}
+
+	public void Update(int id, Window item)
+	{
+		throw new System.NotImplementedException();
+	}
+
+	public void Remove(int id)
+	{
+		throw new System.NotImplementedException();
+	}
+}
