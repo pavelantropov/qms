60cf437b18a2e5a231578b3db9cc232aa3966679
Pavel Antropov <p.m.antropov@gmail.com>
2022-08-13 21:45:19 +0400

    Develop a window service

---
+++ src/qms-consoleapp/Services/IWindowService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+using pavelantropov.qms_consoleapp.Entities;
+
+namespace pavelantropov.qms_consoleapp.Services;
+
+public interface IWindowService
+{
+	Window Get(int id);
+	List<Window> GetAll();
+
+	Window Create(int workDayMinutes,
+		List<ServiceType> supportedServiceTypes);
+	void Change(int id, Window window);
+	void Delete(int id);
+}
---
+++ src/qms-consoleapp/Services/WindowService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using pavelantropov.qms_consoleapp.Entities;
+using pavelantropov.qms_consoleapp.Helpers;
+using pavelantropov.qms_consoleapp.Repositories;
+
+namespace pavelantropov.qms_consoleapp.Services;
+
+public class WindowService : IWindowService
+{
+	private readonly IOutputHelper _outputHelper;
+	private readonly IRepository<Window> _repository;
+
+	public WindowService(IRepository<Window> repository, IOutputHelper outputHelper = null)
+	{
+		_repository = repository;
+		_outputHelper = outputHelper ?? new ConsoleHelper();
+	}
+
+	public Window Get(int id) => _repository.Items.Find(x => x.Id == id);
+
+	public List<Window> GetAll()
+	{
+		return new List<Window>(_repository.Items);
+	}
+
+	public Window Create(int workDayMinutes, List<ServiceType> supportedServiceTypes)
+	{
+		var newId = _repository.Items.Any() ? _repository.Items.Max(x => x.Id) + 1 : 1;
+		var newWindow = new Window(workDayMinutes, supportedServiceTypes) { Id = newId };
+		newWindow.TimeLeftEvent += OnTimeLeft;
+		_repository.Items.Add(newWindow);
+		return newWindow;
+	}
+
+	public void Change(int id, Window window)
+	{
+		throw new System.NotImplementedException();
+	}
+
+	public void Delete(int id)
+	{
+		throw new System.NotImplementedException();
+	}
+
+	private void OnTimeLeft(string message)
+	{
+		_outputHelper.Print(message);
+	}
+}
